6528f82c619fdeac6f5293da8f6cc2a6672f6db2
Alyona Kitova <nazumy@bk.ru>
2023-04-15 12:54:03 +0600

    [+] init

---
+++ app/src/main/java/com/ifeor/weather_outside/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ifeor.weather_outside
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
